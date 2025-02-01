d34640cf87d7c627ab6607199acd72fb4acdb8b0
mark abwoga <markabwoga@gmail.com>
2025-02-01 03:58:20 +0300

    modified Botlist jsx and css

--- src/pages/botList.js
+++ src/pages/botList.js
@@ -1,32 +1,294 @@
 import '../css/BotList.css'
+//images
+import botImage from '../botsfolder/bot1.jpg';
+import botImage2 from '../botsfolder/bot2.jpg';
+import botImage3 from '../botsfolder/bot3.jpg';
+import botImage4 from '../botsfolder/bot4.jpg';
+//import botImage5 from '../botsfolder/bot5.jpg';
+import botImage6 from '../botsfolder/bot6.jpg';
+import botImage7 from '../botsfolder/bot7.jpg';
+import botImage8 from '../botsfolder/bot8.png';
+//import botImage9 from '../botsfolder/bot9.jpg';
+import botImage10 from '../botsfolder/bot10.jpg';
+import botImage11 from '../botsfolder/bot11.jpg';
+import botImage12 from '../botsfolder/bot12.jpg';
+import botImage13 from '../botsfolder/bot13.jpg';
+import botImage14 from '../botsfolder/bot14.jpg';
+import botImage15 from '../botsfolder/bot15.jpg';
+import botImage16 from '../botsfolder/bot16.jpg';
+import botImage17 from '../botsfolder/bot17.jpg';
+//import botImage18 from '../botsfolder/bot18.jpg';
+import botImage19 from '../botsfolder/bot19.jpg';
+import botImage20 from '../botsfolder/bot20.jpg';
+import botImage21 from '../botsfolder/displaybot.jpg';
+import botImage22 from '../botsfolder/medbot.jpg';
+
+//svgs
+import boltIcon from '../svgs/bolt_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg';
+import shiledIcon from '../svgs/security_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg';
+import lifeIcon from '../svgs/ecg_heart_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg';
+
 const BotList = ()=>{
 
     return (
         <div className='botContainer'>
-                <div className='heading'><h1>Select Your Army</h1></div>    
+            <div className='heading'><h1>Select Your Army</h1></div>    
                 <div className='gridContainer'>
-                    <div className="card">bot1</div>
-                    <div className="card">bot2</div>
-                    <div className="card">bot3</div>
-                    <div className="card">bot4</div>
-                    <div className="card">bot5</div>
-                    <div className="card">bot6</div>
-                    <div className="card">bot8</div>
-                    <div className="card">bot9</div>
-                    <div className="card">bot10</div>
-                    <div className="card">bot11</div>
-                    <div className="card">bot12</div>
-                    <div className="card">bot13</div>
-                    <div className="card">bot14</div>
-                    <div className="card">bot15</div>
-                    <div className="card">bot16</div>
-                    <div className="card">bot17</div>
-                    <div className="card">bot18</div>
-                    <div className="card">bot19</div>
-                    <div className="card">bot20</div>
-                    <div className="card">bot21</div>
-
-
+                    <div className="card">
+                        <div className='imageDiv'><img src={botImage} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className='imageDiv'><img src={botImage2} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className='imageDiv'><img src={botImage3} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage4} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage6} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage21} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage7} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage8} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage10} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage11} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage12} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage13} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage14} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage15} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage16} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage17} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage19} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage20} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage22} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage19} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>                    <div className="card">
+                        <div className='imageDiv'><img src={botImage20} alt="Bot" />
+                        </div>
+                        <div className='botInfo'>
+                            <h3>bot1</h3>
+                            <p>01010111010000010</p>
+                        </div>
+                        <div className='botInfoDiv'>
+                            <img src={boltIcon} alt="Power Icon" style={{fill: '#FF5733'}} />73
+                            <img src={shiledIcon} alt="Power Icon" />84
+                            <img src={lifeIcon} alt="Power Icon" />56
+                        </div>
+                    </div>
                 </div>
         </div>
 
